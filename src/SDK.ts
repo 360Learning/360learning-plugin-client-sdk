3c40178399c966c28d499415052a01ecc72b5ebf
EtienneLeco <etienne.lecouturier@360learning.com>
2025-10-24 18:06:47 +0200

    create a method to init sdk

--- src/SDK.ts
+++ src/SDK.ts
@@ -1,1 +1,11 @@
-export class SDK {}
+export class SDK {
+
+    async init() {
+        await this.authenticate();
+    }
+
+    private async authenticate() {
+        // request temporary token & base url
+        // load access token
+    }
+}
