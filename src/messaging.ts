9f79f43159be5510b37b24e6b028e6e70e975e02
EtienneLeco <etienne.lecouturier@360learning.com>
2025-10-26 23:10:45 +0100

    add message emission
    we need to first post a message and wait for the client project to reply.

--- src/messaging.ts
+++ src/messaging.ts
@@ -1,4 +1,5 @@
 const PLUGIN_TEMPORARY_TOKEN_EVENT_TYPE = "plugin:temporaryToken";
+const PLUGIN_HANDSHAKE_EVENT_TYPE = "plugin:handshake";
 
 export type TemporaryTokenPayload = {
     apiBaseUrl: string;
@@ -8,6 +9,8 @@ export type TemporaryTokenPayload = {
 export async function requestTemporaryToken(): Promise<TemporaryTokenPayload> {
     return new Promise((resolve) => {
         window.addEventListener("message", handleMessage, false);
+
+        window.parent.postMessage({ type: PLUGIN_HANDSHAKE_EVENT_TYPE }, "*");
 
         function handleMessage(event: MessageEvent) {
             if (event.data.type !== PLUGIN_TEMPORARY_TOKEN_EVENT_TYPE) { return; }
